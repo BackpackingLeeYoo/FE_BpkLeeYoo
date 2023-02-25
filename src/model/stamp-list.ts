fe865d9f9e9a9ec70bf5b5bbd1f1d6836ab8f307
Milan Lee <yooossi@gmail.com>
2023-02-25 23:33:36 +0900

    [FEAT] 스탬프 리스트 추가

---
+++ src/model/stamp-list.ts
@@ -0,0 +1,82 @@
+export const stampList = [
+  {
+    stampName: "방구집",
+    latitude: 37.486289,
+    longitude: 126.926644,
+  },
+  {
+    stampName: "굴업도", //개머리언덕
+    latitude: 37.186414106277745,
+    longitude: 125.97080924691167,
+  },
+  {
+    stampName: "대매물도", //한산초등학교 매물도 분교장(폐교)
+    latitude: 34.64719624645501,
+    longitude: 128.57647734647284,
+  },
+  {
+    stampName: "개도", //청석포 해수욕장
+    latitude: 34.561353720386826,
+    longitude: 127.66979758338185,
+  },
+  {
+    stampName: "선자령",
+    latitude: 37.72055011675831,
+    longitude: 128.74789586492588,
+  },
+  {
+    stampName: "대이작도", //작은풀안해변 위 펜션
+    latitude: 37.171625142546894,
+    longitude: 126.2585902519401,
+  },
+  {
+    stampName: "비양도", //비양도 항
+    latitude: 33.40680216674114,
+    longitude: 126.2309867595435,
+  },
+  {
+    stampName: "민둥산",
+    latitude: 37.2723910441082,
+    longitude: 128.77541587691616,
+  },
+  {
+    stampName: "박달고치",
+    latitude: 38.027673223081706,
+    longitude: 128.19699774966014,
+  },
+  {
+    stampName: "울릉도", //울릉도 섬 정중앙
+    latitude: 37.50092844469803,
+    longitude: 130.8091955900396,
+  },
+  {
+    stampName: "무의도", //무의도 세렝게티
+    latitude: 37.36576056238703,
+    longitude: 126.41648994613207,
+  },
+  {
+    stampName: "우도", //우도 섬 정중앙
+    latitude: 33.50636642633825,
+    longitude: 126.95284329911533,
+  },
+  {
+    stampName: "도롱이연못",
+    latitude: 37.18904762384216,
+    longitude: 128.81089355028266,
+  },
+  {
+    stampName: "샤랑도", //샤랑도 섬 정중앙
+    latitude: 34.8387946969804,
+    longitude: 128.19339896265637,
+  },
+  {
+    stampName: "노고산",
+    latitude: 37.552999565487596,
+    longitude: 126.94192133440522,
+  },
+  {
+    stampName: "황매산",
+    latitude: 35.49422965955967,
+    longitude: 127.97463757950064,
+  },
+];
