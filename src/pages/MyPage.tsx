68e3d5ca147e668200ccbaf139569908de4c201a
Milan Lee <yooossi@gmail.com>
2023-02-25 23:33:13 +0900

    [FEAT] 마이페이지 미로그인 방어 로직

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -2,10 +2,16 @@ import { Fragment, useEffect } from "react";
 import StampList from "../components/mypage/StampList";
 import Profile from "../components/mypage/Profile";
 import { useGetMyStamp } from "../api/userQuery";
+import isLogin from "../utils/isLogin";
+import { useNavigate } from "react-router-dom";
 
 const MyPage = () => {
   const stampList = useGetMyStamp();
-  console.log(stampList);
+  const navigator = useNavigate();
+
+  if (!isLogin()) {
+    navigator("/sginin");
+  }
 
   return (
     <Fragment>
