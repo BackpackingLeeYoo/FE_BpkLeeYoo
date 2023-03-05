f31406919a8c5c39e0d2c051006f065f0d2ef2c6
Milan Lee <yooossi@gmail.com>
2023-03-05 20:15:04 +0900

    [FIX] useEffect 삭제

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,23 +1,13 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect } from "react";
 import StampList from "../components/mypage/StampList";
 import Profile from "../components/mypage/Profile";
 import { useGetMyStamp } from "../api/userQuery";
 import isLogin from "../utils/isLogin";
 import { useNavigate } from "react-router-dom";
-import { UseQueryResult } from "react-query";
 
 const MyPage = () => {
+  const stampList = useGetMyStamp();
   const navigator = useNavigate();
-  const [stampList, setStampList] = useState<UseQueryResult>();
-
-  const getMyStamp = () => {
-    const data = useGetMyStamp();
-    setStampList(data);
-  };
-
-  useEffect(() => {
-    getMyStamp();
-  }, []);
 
   if (!isLogin()) {
     navigator("/sginin");
